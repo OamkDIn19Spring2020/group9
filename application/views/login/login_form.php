a81952e68bc431443562b3675ec10234a201663b
Alan <dasilva.alan@outlook.com>
2020-04-02 07:02:29 +0300

    login done

---
+++ application/views/login/login_form.php
@@ -0,0 +1,13 @@
+<h2>Login Form</h2>
+
+<form class="" action="<?php echo site_url('login/login') ?>" method="post">
+  <label for="username">Username</label><br>
+  <input type="text" id="username" name="username" value="" required><br>
+
+  <label for="password">Password</label><br>
+  <input type="text" id="password" name="password" value="" required><br>
+  <br>
+  <input type="submit" name="" value="Login">
+</form>
+
+<p><a href="#"> Click here to create new account</a></p>
---
+++ application/views/login/signup_form.php
@@ -0,0 +1,10 @@
+<h2>SignUp Form</h2>
+<form class="" action="<?php echo site_url('login/login') ?>" method="post">
+  <label for="username">Username</label><br>
+  <input type="text" id="username" name="username" value="" required><br>
+
+  <label for="password">Password</label><br>
+  <input type="text" id="password" name="password" value="" required><br>
+  <br>
+  <input type="submit" name="" value="Login">
+</form>
