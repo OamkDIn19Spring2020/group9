371f3f0af76c2c0423bb7479b852de06e13de137
Kuismai <32294061+Kuismai@users.noreply.github.com>
2020-04-08 20:39:15 +0300

    Added display to campgrounds page

--- application/views/camp/show_campgrounds.php
+++ application/views/camp/show_campgrounds.php
@@ -7,3 +7,29 @@
 <p>
   The site url is <?php echo site_url(); ?>
 </p>
+
+
+<?php
+$query = $this->db->query("SELECT * from campground;");
+
+echo "<table>";
+echo "<th>Owner</th> <th>Campsite</th> <th>Description</th> <th>Price / night</th>";
+foreach ($query->result() as $row)
+{
+  $user = $row->user_name;
+  $name = $row->name;
+  $descr = $row->description;
+  $price = $row->price;
+  echo (
+    "<tr>" .
+    "<td>" . $user . "</td>" .
+    "<td>" . $name . "</td>" .
+    "<td>" . $descr . "</td>" .
+    "<td>" . $price . "</td>" .
+    "</tr>"
+
+  );
+}
+
+echo "</table>";
+?>
