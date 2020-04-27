5f8793139c0efe2c98d2d6db736479009b33c8f7
Kuismai <32294061+Kuismai@users.noreply.github.com>
2020-04-27 15:57:15 +0300

    Update show_campgrounds.php
    Added images to campsite listing

--- application/views/camp/show_campgrounds.php
+++ application/views/camp/show_campgrounds.php
@@ -15,16 +15,18 @@ $query = $this->db->query("SELECT * from campground;");
 
 $base = site_url('camp/show_campsite');
 echo "<table>";
-echo "<th>Owner</th> <th>Campsite</th> <th>Description</th> <th>Price / night</th>";
+echo "<th style='width:150px;text-align:center'></th><th style='padding:15px'>Owner</th> <th>Campsite</th> <th>Description</th> <th>Price / night</th>";
 foreach ($query->result() as $row)
 {
   $user = $row->user_name;
   $name = $row->name;
   $descr = $row->description;
   $price = $row->price;
+  $img = $row->img;
   echo (
     "<tr>" .
-    "<td>" . $user . "</td>" .
+    '<td><img style="max-width:100%;height:auto;padding:15px" src="' . $img . '"></td>' .
+    "<td style='padding:15px'>" . $user . "</td>" .
     "<td><a href=\"" . $base . "/" . $name . "\">" . $name . "</a></td>" .
     "<td>" . $descr . "</td>" .
     "<td>" . $price . "</td>" .
