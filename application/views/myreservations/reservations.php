be94391f25c17b498c9f7dc64809764a139e5871
Kuismai <32294061+Kuismai@users.noreply.github.com>
2020-04-27 17:11:30 +0300

    tiny fixes
    reservation bug fix, made some things a little prettier

--- application/views/myreservations/reservations.php
+++ application/views/myreservations/reservations.php
@@ -1,7 +1,7 @@
 <h2>My reservations page</h2>
 
 <?php 
-print_r($reservations);
+//print_r($reservations);
 foreach($reservations as $reservation){
     echo '<div class="card">
             <div class="card-body" style="display: inline-block; justify-content: space-between;">
