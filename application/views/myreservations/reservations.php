aef42b8733762fbeebddab458b9b450794f6db1c
Kuismai <32294061+Kuismai@users.noreply.github.com>
2020-04-28 18:34:15 +0300

    Reservation prettiening

--- application/views/myreservations/reservations.php
+++ application/views/myreservations/reservations.php
@@ -2,10 +2,15 @@
 
 <?php 
 //print_r($reservations);
+
 foreach($reservations as $reservation){
-    echo '<div class="card">
+  $id = $reservation["camp_id"];
+  $query = $this->db->query('SELECT name from campground WHERE camp_id = ' . $id .';');
+  $row = $query->row();
+  $name = $row->name;
+  echo '<div class="card">
             <div class="card-body" style="display: inline-block; justify-content: space-between;">
-            '.$reservation["reservation_id"].' '.$reservation["camp_id"].' '.$reservation["start"].' '.$reservation["end"].' '.$reservation["price"].' 
+            Reservation ID: '.$reservation["reservation_id"].' at campsite '.$name.'. Reserved '.$reservation["start"].' to '.$reservation["end"].'. Total price: '.$reservation["price"].'. 
             </div>
           </div>';
 }
